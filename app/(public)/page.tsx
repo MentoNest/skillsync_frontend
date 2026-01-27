e4c9959773758b9cd61c51579cb0fa7a9b8bd604
Iwueseiter <preciouschia19@gmail.com>
2026-01-27 12:32:01 +0100

    implement call to action section

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -2,6 +2,7 @@ import Hero from '@/components/Hero';
 import SkillSyncFeatures from '@/components/SkillSyncFeatures';
 import LearningPathSection from '@/components/LearningPathSection';
 import Footer from "@/components/Footer";
+import CallToAction from '@/components/CallToAction';
 
 export default function HomePage() {
   return (
@@ -12,6 +13,7 @@ export default function HomePage() {
       </p>
 
       <Hero />
+      <CallToAction />
       <SkillSyncFeatures />
       <LearningPathSection />
       <Footer />
