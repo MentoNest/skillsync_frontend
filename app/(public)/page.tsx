cc65e7d72099143c0274203e0c3dc798b1a7a5fb
shamoo53 <aliyushamsu16@gmail.com>
2026-01-28 20:55:16 +0100

    designed and implemented the testimonial section
    designed and implemented the testimonial section

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,18 +1,18 @@
-import Hero from '@/components/Hero';
-import SkillSyncFeatures from '@/components/SkillSyncFeatures';
-import LearningPathSection from '@/components/LearningPathSection';
+import Hero from "@/components/Hero";
+import SkillSyncFeatures from "@/components/SkillSyncFeatures";
+import LearningPathSection from "@/components/LearningPathSection";
 import Footer from "@/components/Footer";
-import CallToAction from '@/components/CallToAction';
+import CallToAction from "@/components/CallToAction";
+import TestimonialsSection from "@/components/Testimonialssection";
 
 export default function HomePage() {
   return (
-    <div >
-    
-
+    <div>
       <Hero />
       <CallToAction />
       <SkillSyncFeatures />
       <LearningPathSection />
+      <TestimonialsSection />
       <Footer />
     </div>
   );
