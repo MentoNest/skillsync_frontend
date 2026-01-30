18b2bc371629be6e2a8a03838c7a2a2bf012d0b9
Divine Ifediorah <d-ifediorah@leadway.com>
2026-01-30 21:57:58 +0100

    mentor discover section

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -4,6 +4,7 @@ import LearningPathSection from "@/components/LearningPathSection";
 import Footer from "@/components/Footer";
 import CallToAction from "@/components/CallToAction";
 import TestimonialsSection from "@/components/Testimonialssection";
+import { MentorShowcase } from "@/components/mentorShowcase";
 
 export default function HomePage() {
   return (
@@ -12,6 +13,8 @@ export default function HomePage() {
       <CallToAction />
       <SkillSyncFeatures />
       <LearningPathSection />
+      <MentorShowcase />
+    
       <TestimonialsSection />
       <Footer />
     </div>
