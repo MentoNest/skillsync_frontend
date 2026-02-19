df7baeb3b63d6d9e692b8eb7a680bb84889c97fd
Nafiu Ishaq <63783380+nafiuishaaq@users.noreply.github.com>
2026-02-19 14:29:59 +0100

    making few changes

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,22 +1,8 @@
-import Hero from "@/components/Hero";
-import SkillSyncFeatures from "@/components/SkillSyncFeatures";
-import LearningPathSection from "@/components/LearningPathSection";
-import Footer from "@/components/Footer";
-import CallToAction from "@/components/CallToAction";
-import TestimonialsSection from "@/components/Testimonialssection";
-import { MentorShowcase } from "@/components/mentorShowcase";
 
 export default function HomePage() {
   return (
     <div>
-      <Hero />
-      <CallToAction />
-      <SkillSyncFeatures />
-      <LearningPathSection />
-      <MentorShowcase />
-    
-      <TestimonialsSection />
-      <Footer />
+      
     </div>
   );
 }
