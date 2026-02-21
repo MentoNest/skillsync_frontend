ff4be49e425096178a0ccbb93d71e282451b1e46
Big-cedar <g-ekoh@leadway-group.com>
2026-02-22 00:04:17 +0100

    feat: add Why Choose SkillSync

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/hero";
+import WhyChoose from "../components/why-choose";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen">
       <Hero />
+      <WhyChoose />
     </main>
   );
 }
