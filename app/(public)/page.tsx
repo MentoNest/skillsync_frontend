eaf029a96fdd401d60d36a845fde3e808fabc4e1
Big-cedar <g-ekoh@leadway-group.com>
2026-02-24 01:15:08 +0100

    feat: add Quick Access resource cards section to landing page

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/hero";
 import WhyChoose from "../components/why-choose";
+import QuickAccess from "../components/quick-access";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen">
       <Hero />
       <WhyChoose />
+      <QuickAccess />
     </main>
   );
 }
