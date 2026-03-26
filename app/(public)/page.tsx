0841c54126cfd7b99aa851c1fc29f4499b3e0825
kamaldeen Aliyu <kamalaliyu503@gmail.com>
2026-03-26 12:18:02 +0100

    Created and Implemented the Statistics Section

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -2,11 +2,13 @@
 
 import HeroSection from '@/components/landing/HeroSection';
 import WhyChooseUsSection from '@/components/landing/WhyChooseUsSection';
+import PlatformStatisticsSection from '@/components/landing/PlatformStatisticsSection';
 
 export default function PublicHomePage() {
   return (
     <main>
       <HeroSection />
+      <PlatformStatisticsSection />
       <WhyChooseUsSection />
     </main>
   );
