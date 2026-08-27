33d3ecfd48ca5c7f545692310c208533b0e7569e
sadeeq6400 <abk000064@gmail.com>
2026-08-27 10:38:36 +0100

    feat: implement footer and header

---
+++ components/landing/Header.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="bg-white dark:bg-gray-900">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <nav className="flex items-center justify-between">
+          <div>
+            <Link href="/" className="flex items-center">
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                SkillSync
+              </span>
+            </Link>
+          </div>
+          <div>
+            <Link
+              href="/login"
+              className="text-gray-500 dark:text-gray-400 hover:underline"
+            >
+              Login
+            </Link>
+            <Link
+              href="/register"
+              className="ml-4 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+            >
+              Sign Up
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
