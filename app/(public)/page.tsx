7af08fe90a2c16d82f5a07d56f0c0166e60310cb
olatundefay-prog <olatundefay@gmail.com>
2026-08-28 11:02:33 +0000

    Implement resource search filtering

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedMentorHighlight from "@/components/landing/FeaturedMentorHighlight";
 import MentorDiscoverySection from "@/components/landing/MentorDiscoverySection";
 import CTASection from "@/components/landing/CTASection";
+import LearningPathSection from "@/components/landing/LearningPathSection";
 import Image from "next/image";
 
 export default function Home() {
@@ -46,6 +47,7 @@ export default function Home() {
         </div>
       </section>
       <MentorDiscoverySection />
+      <LearningPathSection />
       <FeaturedMentorHighlight />
       <CTASection />
     </main>
