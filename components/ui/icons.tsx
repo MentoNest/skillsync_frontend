74031dfad92d7d26a8fb8f6a23cb288d34e5c0c6
Abdullahi Lateefat <abdullahilateefat03@gmail.com>
2026-08-28 19:40:36 +0100

    feat: implement quick access section layout

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -74,6 +74,46 @@ export const PathIcon = React.memo(function PathIcon(props: IconProps) {
   );
 });
 
+export const FileTextIcon = React.memo(function FileTextIcon(props: IconProps) {
+  return (
+    <svg {...baseProps} {...props}>
+      <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
+      <polyline points="14 2 14 8 20 8" />
+      <line x1="16" y1="13" x2="8" y2="13" />
+      <line x1="16" y1="17" x2="8" y2="17" />
+      <polyline points="10 9 9 9 8 9" />
+    </svg>
+  );
+});
+
+export const PlayCircleIcon = React.memo(function PlayCircleIcon(props: IconProps) {
+  return (
+    <svg {...baseProps} {...props}>
+      <circle cx="12" cy="12" r="10" />
+      <polygon points="10 8 16 12 10 16 10 8" />
+    </svg>
+  );
+});
+
+export const BookOpenIcon = React.memo(function BookOpenIcon(props: IconProps) {
+  return (
+    <svg {...baseProps} {...props}>
+      <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
+      <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
+    </svg>
+  );
+});
+
+export const DownloadIcon = React.memo(function DownloadIcon(props: IconProps) {
+  return (
+    <svg {...baseProps} {...props}>
+      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+      <polyline points="7 10 12 15 17 10" />
+      <line x1="12" y1="15" x2="12" y2="3" />
+    </svg>
+  );
+});
+
 export type IconName =
   | "path"
   | "mentor"
